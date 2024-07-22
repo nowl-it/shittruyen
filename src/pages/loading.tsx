f2ea0e38c62abc1dec237c83ed36462d29b45dfc
huyhoangg9owl <thh241005@gmail.com>
2024-07-23 03:04:55 +0700

    test build 21

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -1,17 +1,15 @@
-import { VariantProps } from "class-variance-authority";
-import { forwardRef, HTMLAttributes } from "react";
+import { forwardRef, HTMLAttributes } from 'react';
 
-export interface LoadingProps
-    extends HTMLAttributes<HTMLDivElement> {
-    asChild?: boolean
+export interface LoadingProps extends HTMLAttributes<HTMLDivElement> {
+	asChild?: boolean;
 }
 
 const Loading = forwardRef<HTMLDivElement, LoadingProps>((props, ref) => {
-    return (
-        <div className="size-full content-center text-center col-span-full row-span-full" ref={ref}>
-            <h1 className="text-2xl">Cuttruyen Is Suck</h1>
-        </div>
-    );
-})
+	return (
+		<div className="col-span-full row-span-full size-full content-center text-center" ref={ref}>
+			<h1 className="text-2xl">Cuttruyen Is Suck</h1>
+		</div>
+	);
+});
 
 export default Loading;
