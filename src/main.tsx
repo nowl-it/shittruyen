5592f851a822c9cf84d4eb396f9f412cfe71ece3
huyhoangg9owl <thh241005@gmail.com>
2024-07-23 02:59:37 +0700

    test build 20

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import App from '@/App';
+import ThemeConfig from 'global/themes.conf.json';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import ThemeConfig from '@global/themes.conf.json';
-import App from '@/App';
 
 import '@/globals.css';
 import { ThemeProvider } from 'next-themes';
@@ -13,17 +13,17 @@ const { theme: defaultTheme } = general.default;
 const { themes } = resource;
 
 createRoot(document.getElementById('__shittruyen') as HTMLElement).render(
-    <StrictMode>
-        <HelmetProvider>
-            <ThemeProvider
-                defaultTheme={defaultTheme.value}
-                themes={themes.map((theme: any) => theme.value)}
-                attribute="class"
-            >
-                <BrowserRouter>
-                    <App />
-                </BrowserRouter>
-            </ThemeProvider>
-        </HelmetProvider>
-    </StrictMode>
+	<StrictMode>
+		<HelmetProvider>
+			<ThemeProvider
+				defaultTheme={defaultTheme.value}
+				themes={themes.map((theme: any) => theme.value)}
+				attribute="class"
+			>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ThemeProvider>
+		</HelmetProvider>
+	</StrictMode>
 );
