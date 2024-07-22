39170a42263ff4e9faaf493537fc17cba0641b7a
huyhoangg9owl <thh241005@gmail.com>
2024-07-23 02:57:54 +0700

    test build 18

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,25 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig(
-    async () =>
-        ({
-            plugins: [react()],
-            clearScreen: false,
-            server: {
-                port: 1420,
-                strictPort: true
-            },
-            envPrefix: ['VITE_', 'TAURI_'],
-            build: {
-                // Tauri supports es2021
-                target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
-                // don't minify for debug builds
-                minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-                // produce sourcemaps for debug builds
-                sourcemap: !!process.env.TAURI_DEBUG
-            },
-            resolve: {
-                alias: {
-                    '@global': path.resolve(__dirname, './'),
-                    '@': path.resolve(__dirname, './src')
-                }
-            }
-        }) as any
+	async () =>
+		({
+			plugins: [react()],
+			clearScreen: false,
+			server: {
+				port: 1420,
+				strictPort: true
+			},
+			envPrefix: ['VITE_', 'TAURI_'],
+			build: {
+				target: process.env.TAURI_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+				minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+				sourcemap: !!process.env.TAURI_DEBUG
+			},
+			resolve: {
+				alias: {
+					global: path.resolve(__dirname, './'),
+					'@': path.resolve(__dirname, './src')
+				}
+			}
+		}) as any
 );
