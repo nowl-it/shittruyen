72f27a7a06c39f24c739c5122c7da87e4b136c7f
huyhoangg9owl <thh241005@gmail.com>
2024-08-03 04:59:55 +0700

    Remove "import * as React from 'react'"

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,10 +3,8 @@ import axios, { AxiosRequestConfig } from 'axios';
 const { VITE_PROXY_API, VITE_BASE_API } = import.meta.env;
 
 export const AxiosInstance = async (config: AxiosRequestConfig<any> = {}) => {
-	const instance = axios.create({
+	return axios.create({
 		baseURL: VITE_PROXY_API + VITE_BASE_API,
 		...config
 	});
-
-	return instance;
 };
--- src/components/ui/hover-card.tsx
+++ src/components/ui/hover-card.tsx
@@ -1,6 +1,5 @@
 import * as HoverCardPrimitive from '@radix-ui/react-hover-card';
-import * as React from 'react';
-
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
 import { cn } from '@/lib/utils';
 
 const HoverCard = HoverCardPrimitive.Root;
@@ -9,22 +8,30 @@ const HoverCardPortal = HoverCardPrimitive.Portal;
 
 const HoverCardTrigger = HoverCardPrimitive.Trigger;
 
-const HoverCardContent = React.forwardRef<
-	React.ElementRef<typeof HoverCardPrimitive.Content>,
-	React.ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content> & { hoverDisabled?: boolean }
->(({ className, align = 'center', sideOffset = 4, hoverDisabled = false, ...props }, ref) =>
-	hoverDisabled ? null : (
-		<HoverCardPrimitive.Content
-			ref={ref}
-			align={align}
-			sideOffset={sideOffset}
-			className={cn(
-				'z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-				className
-			)}
-			{...props}
-		/>
-	)
+const HoverCardContent = forwardRef<
+        ElementRef<typeof HoverCardPrimitive.Content>,
+        ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content> & {
+    hoverDisabled?: boolean
+}
+>(({
+       className,
+       align = 'center',
+       sideOffset = 4,
+       hoverDisabled = false,
+       ...props
+   }, ref) =>
+        hoverDisabled ? null : (
+                <HoverCardPrimitive.Content
+                        ref={ref}
+                        align={align}
+                        sideOffset={sideOffset}
+                        className={cn(
+                                'z-50 w-64 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+                                className
+                        )}
+                        {...props}
+                />
+        )
 );
 HoverCardContent.displayName = HoverCardPrimitive.Content.displayName;
 
