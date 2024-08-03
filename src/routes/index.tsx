97b21d9eb270eb3c18f044d432e3e8fe34704beb
huyhoangg9owl <thh241005@gmail.com>
2024-08-03 14:51:02 +0700

    Fk my life

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,35 +1,35 @@
 import { HomeAPI } from '@/api/home';
 import { Skeleton } from '@/components/ui/skeleton';
+import { MangaListCard, Spotlight } from '@/pages/home';
 import { createFileRoute } from '@tanstack/react-router';
 import { Fragment } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { MangaListCard, Spotlight } from '@/pages/home';
 
 export const Route = createFileRoute('/')({
-  loader: HomeAPI,
-  pendingComponent: () => <Skeleton className="mx-auto h-80 w-4/5" />,
-  errorComponent: ({ error }) => {
-    return <div>{error.message}</div>;
-  },
-  component: Index
+	loader: HomeAPI,
+	pendingComponent: () => <Skeleton className="mx-auto h-80 w-4/5" />,
+	errorComponent: ({ error }) => {
+		return <div>{error.message}</div>;
+	},
+	component: Index
 });
 
 function Index() {
-  const {
-    data: { spotlight_mangas, new_chapter_mangas }
-  } = Route.useLoaderData();
+	const {
+		data: { spotlight_mangas, new_chapter_mangas }
+	} = Route.useLoaderData();
 
-  return (
-    <Fragment>
-      <Helmet title="Trang chủ" />
-      <section className="mb-6">
-        <Spotlight data={spotlight_mangas} />
-      </section>
+	return (
+		<Fragment>
+			<Helmet title="Trang chủ" />
+			<section className="mb-6">
+				<Spotlight data={spotlight_mangas} />
+			</section>
 
-      <section className="mb-6 w-full px-4">
-        <h2 className="mb-4 text-2xl font-semibold">Mới cập nhật</h2>
-        <MangaListCard data={new_chapter_mangas} />
-      </section>
-    </Fragment>
-  );
+			<section className="mb-6 w-full px-4">
+				<h2 className="mb-4 text-2xl font-semibold">Mới cập nhật</h2>
+				<MangaListCard data={new_chapter_mangas} />
+			</section>
+		</Fragment>
+	);
 }
