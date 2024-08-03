a9df2723405184386bcf91f5c370064d9aa742be
huyhoangg9owl <thh241005@gmail.com>
2024-08-03 07:14:23 +0700

    Removing Qodana from WebStorm

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import '@/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 import ThemeConfig from 'global/themes.conf.json';
 import { ThemeProvider } from 'next-themes';
 import { StrictMode } from 'react';
@@ -20,48 +20,48 @@ const { theme: defaultTheme } = general.default;
 const { themes } = resource;
 
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			refetchOnWindowFocus: false
-		}
-	}
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false
+        }
+    }
 });
 
 const router = createRouter({
-	routeTree,
-	context: {
-		queryClient
-	},
-	defaultPreload: 'intent',
-	defaultPreloadStaleTime: 0,
-	defaultPendingMs: 1000,
-	defaultPendingComponent: Loading,
-	defaultNotFoundComponent: NotFound,
-	defaultErrorComponent: Error
+    routeTree,
+    context: {
+        queryClient
+    },
+    defaultPreload: 'intent',
+    defaultPreloadStaleTime: 0,
+    defaultPendingMs: 1000,
+    defaultPendingComponent: Loading,
+    defaultNotFoundComponent: NotFound,
+    defaultErrorComponent: Error
 });
 
 declare module '@tanstack/react-router' {
-	interface Register {
-		router: typeof router;
-	}
+    interface Register {
+        router: typeof router;
+    }
 }
 const rootElement = document.getElementById('__shittruyen')!;
 
-if (!rootElement.innerHTML) {
-	const root = createRoot(rootElement);
-	root.render(
-		<StrictMode>
-			<QueryClientProvider client={queryClient}>
-				<HelmetProvider>
-					<ThemeProvider
-						defaultTheme={defaultTheme.value}
-						themes={themes.map((theme: any) => theme.value)}
-						attribute="class"
-					>
-						<RouterProvider router={router} />
-					</ThemeProvider>
-				</HelmetProvider>
-			</QueryClientProvider>
-		</StrictMode>
-	);
+if (!rootElement) {
+    const root = createRoot(rootElement);
+    root.render(
+            <StrictMode>
+                <QueryClientProvider client={queryClient}>
+                    <HelmetProvider>
+                        <ThemeProvider
+                                defaultTheme={defaultTheme.value}
+                                themes={themes.map((theme: any) => theme.value)}
+                                attribute="class"
+                        >
+                            <RouterProvider router={router} />
+                        </ThemeProvider>
+                    </HelmetProvider>
+                </QueryClientProvider>
+            </StrictMode>
+    );
 }
